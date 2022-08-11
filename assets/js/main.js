0f336df7999dd6fffe1103e41fa8311a77be3f55
HernanS99 <hernan.silva2299@gmail.com>
2022-08-11 19:08:13 -0400

    parte grafica lista

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const productos = [];
+
+
+const insert = () =>{
+    producto = {
+        name: "weed",
+        desc: "descrpcion",
+        price: "13.000"
+    }
+    productos.push(producto)
+    console.log(productos)
+}
+
+insert()
