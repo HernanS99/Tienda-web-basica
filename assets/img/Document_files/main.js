b81fbe8cd0ddb64191bf6ed56051bf902647506e
HernanS99 <hernan.silva2299@gmail.com>
2022-08-12 14:44:16 -0400

    insert y read listos (faltan validaciones)

---
+++ assets/img/Document_files/main.js
@@ -0,0 +1,31 @@
+const productos = [];
+
+document.querySelector("#btnagregar").addEventListener("click",validarSemilla)
+
+function validarSemilla (){
+    
+    let name = document.getElementById("nombre").value;
+    let desc = document.getElementById("desc").value;
+    let precio = document.getElementById("precio").value;
+
+    if(nombre === "" || desc === ""|| precio === "")
+    {
+        alert("No olvides rellenar todos los campos")
+    } else {
+        producto = {
+            name,
+            desc,
+            precio
+        }
+    }
+}
+
+
+
+function insert(producto1){
+    productos.push(producto1);
+    console.log(productos);
+    localStorage.setItem("weed", JSON.stringify(productos));
+}
+
+insert()
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,28 @@
-const productos = [];
+let productos = [];
+let producto = {}
 
-document.querySelector("#btnagregar").addEventListener("click",validarSemilla)
+document.querySelector("#btnagregar").addEventListener("click", validarSemilla)
 
-function validarSemilla (){
-    
+function print() {
+    let filas = document.getElementById("filas");
+    filas.innerHTML = '';
+    productos.forEach(producto=>
+    filas.innerHTML += `
+        <tr>
+                <td>${producto.name}</td>
+                <td>${producto.desc}</td>
+                <td>${producto.precio}</td>
+        </tr>`
+    )
+}
+
+function validarSemilla() {
+
     let name = document.getElementById("nombre").value;
     let desc = document.getElementById("desc").value;
     let precio = document.getElementById("precio").value;
 
-    if(nombre === "" || desc === ""|| precio === "")
-    {
+    if (nombre === "" || desc === "" || precio === "") {
         alert("No olvides rellenar todos los campos")
     } else {
         producto = {
@@ -17,16 +30,27 @@ function validarSemilla (){
             desc,
             precio
         }
-        insert(producto);
+        insert(producto)
     }
 }
 
 
 
-function insert(producto1){
+function insert(producto1) {//create
     productos.push(producto1);
-    console.log(productos);
     localStorage.setItem("weed", JSON.stringify(productos));
 }
 
-insert()
+function getProducts() {//conseguir datos
+    let listaweed = localStorage.getItem('weed')
+    productos = !listaweed ? [] : JSON.parse(listaweed);
+    print()
+    return productos;
+    
+}
+
+
+
+
+
+getProducts();
